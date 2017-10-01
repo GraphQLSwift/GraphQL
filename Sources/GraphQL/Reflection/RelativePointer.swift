6a931d040793e71a576ed8457d8aa449b5e59636
Jeff Seibert <jeff@jseibert.com>
2017-09-30 21:34:37 -0500

    Adopt Reflection 0.15.0

--- Sources/GraphQL/Reflection/RelativePointer.swift
+++ Sources/GraphQL/Reflection/RelativePointer.swift
@@ -1,8 +1,5 @@
-
-#if swift(>=4)
 func relativePointer<T, U, V>(base: UnsafePointer<T>, offset: U) -> UnsafePointer<V> where U : BinaryInteger {
-    let p = UnsafeRawPointer(base).assumingMemoryBound(to: Int8.self)
-    return UnsafeRawPointer(p.advanced(by: Int(integer: offset))).assumingMemoryBound(to: V.self)
+    return UnsafeRawPointer(base).advanced(by: Int(integer: offset)).assumingMemoryBound(to: V.self)
 }
 
 extension Int {
@@ -16,24 +13,3 @@ extension Int {
         }
     }
 }
-
-#else
-
-func relativePointer<T, U, V>(base: UnsafePointer<T>, offset: U) -> UnsafePointer<V> where U : Integer {
-    let p = UnsafeRawPointer(base).assumingMemoryBound(to: Int8.self)
-    return UnsafeRawPointer(p.advanced(by: Int(integer: offset))).assumingMemoryBound(to: V.self)
-}
-
-extension Int {
-    fileprivate init<T : Integer>(integer: T) {
-        switch integer {
-        case let value as Int: self = value
-        case let value as Int32: self = Int(value)
-        case let value as Int16: self = Int(value)
-        case let value as Int8: self = Int(value)
-        default: self = 0
-        }
-    }
-}
-
-#endif
