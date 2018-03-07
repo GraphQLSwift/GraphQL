33b40db16a419d5b63eb4ef1a37f4e91179c7280
Jeff Seibert <jeff@jseibert.com>
2018-03-06 19:38:01 -0800

    Publish library in Package.swift

--- Package.swift
+++ Package.swift
@@ -3,14 +3,18 @@ import PackageDescription
 
 let package = Package(
     name: "GraphQL",
-    
+
+    products: [
+        .library(name: "GraphQL", targets: ["GraphQL"]),
+    ],
+
     dependencies: [
         .package(url: "https://github.com/wickwirew/Runtime.git", .branch("swift-4.1")),
     ],
-    
+
     targets: [
         .target(name: "GraphQL", dependencies: ["Runtime"]),
-        
+
         .testTarget(name: "GraphQLTests", dependencies: ["GraphQL"]),
     ]
 )
