2920ee6cc3098390f55a671e805432a77ca2ebbf
Mike Pollard <mike.pollard@clubspark.com>
2019-01-23 22:35:25 +0000

    Fix StarWarsIntrospectionTets.testIntrospectionTypeQuery on linux, upgrade travis to xcode10.1

--- Tests/GraphQLTests/StarWarsTests/StarWarsIntrospectionTests.swift
+++ Tests/GraphQLTests/StarWarsTests/StarWarsIntrospectionTests.swift
@@ -19,7 +19,6 @@ class StarWarsIntrospectionTests : XCTestCase {
                     "    }" +
                     "}"
 
-        #if os(macOS)
         let expected: Map = [
             "data": [
                 "__schema": [
@@ -73,61 +72,6 @@ class StarWarsIntrospectionTests : XCTestCase {
                 ],
             ],
         ]
-        #else
-        let expected: Map = [
-            "data": [
-                "__schema": [
-                    "types": [
-                        [
-                            "name": "__Directive",
-                        ],
-                        [
-                            "name": "__DirectiveLocation",
-                        ],
-                        [
-                            "name": "__EnumValue",
-                        ],
-                        [
-                            "name": "__Field",
-                        ],
-                        [
-                            "name": "__InputValue",
-                        ],
-                        [
-                            "name": "__Schema",
-                        ],
-                        [
-                            "name": "__Type",
-                        ],
-                        [
-                            "name": "__TypeKind",
-                        ],
-                        [
-                            "name": "Boolean",
-                        ],
-                        [
-                            "name": "Character",
-                        ],
-                        [
-                            "name": "Droid",
-                        ],
-                        [
-                            "name": "Episode",
-                        ],
-                        [
-                            "name": "Human",
-                        ],
-                        [
-                            "name": "Query",
-                        ],
-                        [
-                            "name": "String",
-                        ],
-                    ],
-                ],
-            ],
-        ]
-        #endif
 
             let result = try graphql(schema: StarWarsSchema, request: query, eventLoopGroup: eventLoopGroup).wait()
             XCTAssertEqual(result, expected)
