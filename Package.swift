140722debf3e17aef713c0245a6229518c1f8e8e
Mike Pollard <mike.pollard@clubspark.com>
2019-04-25 11:13:09 +0100

    Use nested-struct-crash branch of Runtime

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(url: "https://github.com/wickwirew/Runtime.git", from: "2.0.0"),
+        .package(url: "https://github.com/wickwirew/Runtime.git", .branch("nested-struct-crash")),
 
         // ⏱ Promises and reactive-streams in Swift built for high-performance and scalability.
         .package(url: "https://github.com/vapor/core.git", from: "3.0.0"),
