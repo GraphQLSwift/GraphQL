649b14846a49f6fc5905cb0b287b84ca22c4c867
Mathias Quintero <me@quintero.io>
2020-03-21 15:14:37 +0100

    Using my version of Runtime

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.0.0")),
-        .package(url: "https://github.com/wickwirew/Runtime.git", .upToNextMinor(from: "2.1.0"))
+        .package(url: "https://github.com/nerdsupremacist/Runtime.git", from: "2.1.2-beta.1")
     ],
     targets: [
         .target(name: "GraphQL", dependencies: ["NIO", "Runtime"]),
