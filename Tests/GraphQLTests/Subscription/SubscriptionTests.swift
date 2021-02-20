7f3f60a0706d6d77c62608ce61f42f8ae7bd2fb9
Jay Herron <jay@passivelogic.com>
2021-02-19 18:26:39 -0700

    refactors helpers, adds more tests

--- Tests/GraphQLTests/Subscription/SubscriptionTests.swift
+++ Tests/GraphQLTests/Subscription/SubscriptionTests.swift
@@ -6,65 +6,11 @@ import RxSwift
 
 class SubscriptionTests : XCTestCase {
     
-    /// Creates a subscription result for the input pub/sub, schema, and AST document
-    private func createSubscription(
-        pubsub:Observable<Any>,
-        schema:GraphQLSchema? = nil,
-        document:Document = defaultSubscriptionAST
-    ) throws -> Observable<GraphQLResult> {
-        
-        var emails = [
-            Email(
-                from: "joe@graphql.org",
-                subject: "Hello",
-                message: "Hello World",
-                unread: false
-            )
-        ]
-        
-        let testSchema = schema ?? emailSchemaWithResolvers(
-            subscribe: {_, _, _, eventLoopGroup, _ throws -> EventLoopFuture<Any?> in
-                return eventLoopGroup.next().makeSucceededFuture(pubsub)
-            },
-            resolve: {emailAny, _, _, eventLoopGroup, _ throws -> EventLoopFuture<Any?> in
-                let email = emailAny as! Email
-                emails.append(email)
-                return eventLoopGroup.next().makeSucceededFuture(EmailEvent(
-                    email: email,
-                    inbox: Inbox(emails: emails)
-                ))
-            }
-        )
-        
-        let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
-        
-        let subscriptionResult = try subscribe(
-            queryStrategy: SerialFieldExecutionStrategy(),
-            mutationStrategy: SerialFieldExecutionStrategy(),
-            subscriptionStrategy: SerialFieldExecutionStrategy(),
-            instrumentation: NoOpInstrumentation,
-            schema: testSchema,
-            documentAST: document,
-            rootValue: Void(),
-            context: Void(),
-            eventLoopGroup: eventLoopGroup,
-            variableValues: [:],
-            operationName: nil
-        ).wait()
-        
-        switch subscriptionResult {
-        case .success(let subscription):
-            return subscription
-        case .failure(let error):
-            throw error
-        }
-    }
-    
     // MARK: Basic test to see if publishing is working
     func testBasic() throws {
         let disposeBag = DisposeBag()
         let pubsub = PublishSubject<Any>()
-        let subscription = try createSubscription(pubsub: pubsub)
+        let subscription = try createDbAndSubscription(pubsub: pubsub, query: defaultQuery)
         
         let expected = GraphQLResult(
             data: ["importantEmail": [
@@ -93,114 +39,137 @@ class SubscriptionTests : XCTestCase {
     // MARK: Subscription Initialization Phase
 
     /// accepts multiple subscription fields defined in schema
-//    func testAcceptsMultipleSubscriptionFields() throws {
-//        let pubsub = PublishSubject<Any>()
-//        let subscriptionTypeMultiple = try GraphQLObjectType(
-//            name: "Subscription",
-//            fields: [
-//                "importantEmail": GraphQLField (type: EmailEventType),
-//                "notImportantEmail": GraphQLField (type: EmailEventType)
-//            ]
-//        )
-//        let testSchema = try GraphQLSchema(
-//            query: EmailQueryType,
-//            subscription: subscriptionTypeMultiple
-//        )
-//        let subscription = try createSubscription(pubsub: pubsub, schema: testSchema)
-//        pubsub.onNext(Email(
-//            from: "yuzhi@graphql.org",
-//            subject: "Alright",
-//            message: "Tests are good",
-//            unread: true
-//        ))
-//    }
-//
-//
-//    // TODO Not working. I think it's because it's checking the Resolver return against the Schema-defined return type...
-//    func testResolverReturningErrorSchema() throws {
-//        let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
-//        let schema = emailSchemaWithResolvers(
-//            subscribe: {_, _, _, eventLoopGroup, _ throws -> EventLoopFuture<Any?> in
-//                return eventLoopGroup.next().makeSucceededFuture(GraphQLError(message: "test error"))
-//            },
-//            resolve: nil
-//        )
-//        let document = try parse(source: """
-//            subscription {
-//                importantEmail
-//            }
-//        """)
-//        let result = try createSourceEventStream(
-//            queryStrategy: SerialFieldExecutionStrategy(),
-//            mutationStrategy: SerialFieldExecutionStrategy(),
-//            subscriptionStrategy: SerialFieldExecutionStrategy(),
-//            instrumentation: NoOpInstrumentation,
-//            schema: schema,
-//            documentAST: document,
-//            rootValue: Void(),
-//            context: Void(),
-//            eventLoopGroup: eventLoopGroup
-//        ).wait()
-//
-//        switch result {
-//        case .success:
-//            XCTFail()
-//        case .failure(let error):
-//            let expected = GraphQLError(message:"test error")
-//            XCTAssertEqual(expected, error)
-//        }
-//    }
-//
-//    // Working!!!
-//    func testResolverThrowingErrorSchema() throws {
-//        let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
-//        let schema = emailSchemaWithResolvers(
-//            subscribe: {_, _, _, eventLoopGroup, _ throws -> EventLoopFuture<Any?> in
-//                throw GraphQLError(message: "test error")
-//            },
-//            resolve: nil
-//        )
-//        let document = try parse(source: """
-//            subscription {
-//                importantEmail
-//            }
-//        """)
-//        let result = try createSourceEventStream(
-//            queryStrategy: SerialFieldExecutionStrategy(),
-//            mutationStrategy: SerialFieldExecutionStrategy(),
-//            subscriptionStrategy: SerialFieldExecutionStrategy(),
-//            instrumentation: NoOpInstrumentation,
-//            schema: schema,
-//            documentAST: document,
-//            rootValue: Void(),
-//            context: Void(),
-//            eventLoopGroup: eventLoopGroup
-//        ).wait()
-//
-//        switch result {
-//        case .success(let observable):
-//            XCTFail()
-//        case .failure(let error):
-//            let expected = GraphQLError(message:"test error")
-//            XCTAssertEqual(expected, error)
-//        }
-//    }
+    func testAcceptsMultipleSubscriptionFields() throws {
+        let disposeBag = DisposeBag()
+        let pubsub = PublishSubject<Any>()
+        
+        var emails = defaultEmails
+        let schema = try GraphQLSchema(
+            query: EmailQueryType,
+            subscription: try! GraphQLObjectType(
+                name: "Subscription",
+                fields: [
+                    "importantEmail": GraphQLField(
+                        type: EmailEventType,
+                        args: [
+                            "priority": GraphQLArgument(
+                                type: GraphQLInt
+                            )
+                        ],
+                        resolve: {emailAny, _, _, eventLoopGroup, _ throws -> EventLoopFuture<Any?> in
+                            let email = emailAny as! Email
+                            emails.append(email)
+                            return eventLoopGroup.next().makeSucceededFuture(EmailEvent(
+                                email: email,
+                                inbox: Inbox(emails: emails)
+                            ))
+                        },
+                        subscribe: {_, _, _, eventLoopGroup, _ throws -> EventLoopFuture<Any?> in
+                            return eventLoopGroup.next().makeSucceededFuture(pubsub)
+                        }
+                    ),
+                    "notImportantEmail": GraphQLField(
+                        type: EmailEventType,
+                        args: [
+                            "priority": GraphQLArgument(
+                                type: GraphQLInt
+                            )
+                        ],
+                        resolve: {emailAny, _, _, eventLoopGroup, _ throws -> EventLoopFuture<Any?> in
+                            let email = emailAny as! Email
+                            emails.append(email)
+                            return eventLoopGroup.next().makeSucceededFuture(EmailEvent(
+                                email: email,
+                                inbox: Inbox(emails: emails)
+                            ))
+                        },
+                        subscribe: {_, _, _, eventLoopGroup, _ throws -> EventLoopFuture<Any?> in
+                            return eventLoopGroup.next().makeSucceededFuture(pubsub)
+                        }
+                    )
+                ]
+            )
+        )
+        let subscription = try createSubscription(pubsub: pubsub, schema: schema, query: defaultQuery)
+        
+        let expected = GraphQLResult(
+            data: ["importantEmail": [
+                "inbox":[
+                    "total": 2,
+                    "unread": 1
+                ],
+                "email":[
+                    "subject": "Alright",
+                    "from": "yuzhi@graphql.org"
+                ]
+            ]]
+        )
+        let _ = subscription.subscribe { event in
+            XCTAssertEqual(event.element, expected)
+        }.disposed(by: disposeBag)
+        pubsub.onNext(Email(
+            from: "yuzhi@graphql.org",
+            subject: "Alright",
+            message: "Tests are good",
+            unread: true
+        ))
+    }
+    
+    /// 'should only resolve the first field of invalid multi-field'
+    func testInvalidMultiField() throws {
+        let disposeBag = DisposeBag()
+        let pubsub = PublishSubject<Any>()
+        
+        var didResolveImportantEmail = false
+        var didResolveNonImportantEmail = false
+        
+        let schema = try GraphQLSchema(
+            query: EmailQueryType,
+            subscription: try! GraphQLObjectType(
+                name: "Subscription",
+                fields: [
+                    "importantEmail": GraphQLField(
+                        type: EmailEventType,
+                        resolve: {_, _, _, eventLoopGroup, _ throws -> EventLoopFuture<Any?> in
+                            return eventLoopGroup.next().makeSucceededFuture(nil)
+                        },
+                        subscribe: {_, _, _, eventLoopGroup, _ throws -> EventLoopFuture<Any?> in
+                            didResolveImportantEmail = true
+                            return eventLoopGroup.next().makeSucceededFuture(pubsub)
+                        }
+                    ),
+                    "notImportantEmail": GraphQLField(
+                        type: EmailEventType,
+                        resolve: {_, _, _, eventLoopGroup, _ throws -> EventLoopFuture<Any?> in
+                            return eventLoopGroup.next().makeSucceededFuture(nil)
+                        },
+                        subscribe: {_, _, _, eventLoopGroup, _ throws -> EventLoopFuture<Any?> in
+                            didResolveNonImportantEmail = true
+                            return eventLoopGroup.next().makeSucceededFuture(pubsub)
+                        }
+                    )
+                ]
+            )
+        )
+        let subscription = try createSubscription(pubsub: pubsub, schema: schema, query: """
+            subscription {
+                importantEmail
+                notImportantEmail
+            }
+        """)
+        
+        let _ = subscription.subscribe().disposed(by: disposeBag)
+        pubsub.onNext(Email(
+            from: "yuzhi@graphql.org",
+            subject: "Alright",
+            message: "Tests are good",
+            unread: true
+        ))
+        
+        XCTAssertTrue(didResolveImportantEmail)
+        XCTAssertFalse(didResolveNonImportantEmail)
+    }
 }
-
-let defaultSubscriptionAST = try! parse(source: """
-    subscription ($priority: Int = 0) {
-        importantEmail(priority: $priority) {
-          email {
-            from
-            subject
-          }
-          inbox {
-            unread
-            total
-          }
-        }
-      }
-""")
 
 // MARK: Types
 struct Email : Encodable {
@@ -279,7 +248,82 @@ let EmailQueryType = try! GraphQLObjectType(
     ]
 )
 
-func emailSchemaWithResolvers(subscribe: GraphQLFieldResolve?, resolve: GraphQLFieldResolve?) -> GraphQLSchema {
+// MARK: Test Helpers
+
+let defaultQuery = """
+    subscription ($priority: Int = 0) {
+        importantEmail(priority: $priority) {
+          email {
+            from
+            subject
+          }
+          inbox {
+            unread
+            total
+          }
+        }
+      }
+"""
+
+let defaultEmails = [
+    Email(
+        from: "joe@graphql.org",
+        subject: "Hello",
+        message: "Hello World",
+        unread: false
+    )
+]
+
+/// Generates a default schema and email database, and returns the subscription
+private func createDbAndSubscription(
+    pubsub:Observable<Any>,
+    query:String
+) throws -> Observable<GraphQLResult> {
+    
+    var emails = defaultEmails
+    
+    let schema = emailSchemaWithResolvers(
+        resolve: {emailAny, _, _, eventLoopGroup, _ throws -> EventLoopFuture<Any?> in
+            let email = emailAny as! Email
+            emails.append(email)
+            return eventLoopGroup.next().makeSucceededFuture(EmailEvent(
+                email: email,
+                inbox: Inbox(emails: emails)
+            ))
+        },
+        subscribe: {_, _, _, eventLoopGroup, _ throws -> EventLoopFuture<Any?> in
+            return eventLoopGroup.next().makeSucceededFuture(pubsub)
+        }
+    )
+    
+    return try createSubscription(pubsub: pubsub, schema: schema, query: query)
+}
+
+/// Generates a subscription from the given schema and query. It's expected that the database is managed by the caller.
+private func createSubscription(
+    pubsub: Observable<Any>,
+    schema: GraphQLSchema,
+    query: String
+) throws -> Observable<GraphQLResult> {
+    let document = try parse(source: query)
+    let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+    let subscriptionOrError = try subscribe(
+        queryStrategy: SerialFieldExecutionStrategy(),
+        mutationStrategy: SerialFieldExecutionStrategy(),
+        subscriptionStrategy: SerialFieldExecutionStrategy(),
+        instrumentation: NoOpInstrumentation,
+        schema: schema,
+        documentAST: document,
+        rootValue: Void(),
+        context: Void(),
+        eventLoopGroup: eventLoopGroup,
+        variableValues: [:],
+        operationName: nil
+    ).wait()
+    return try extractSubscription(subscriptionOrError)
+}
+
+private func emailSchemaWithResolvers(resolve: GraphQLFieldResolve?, subscribe: GraphQLFieldResolve?) -> GraphQLSchema {
     return try! GraphQLSchema(
         query: EmailQueryType,
         subscription: try! GraphQLObjectType(
@@ -298,4 +342,13 @@ func emailSchemaWithResolvers(subscribe: GraphQLFieldResolve?, resolve: GraphQLF
             ]
         )
     )
+}
+
+private func extractSubscription(_ subscriptionResult: SubscriptionResult) throws -> Observable<GraphQLResult> {
+    switch subscriptionResult {
+    case .success(let subscription):
+        return subscription
+    case .failure(let error):
+        throw error
+    }
 }
