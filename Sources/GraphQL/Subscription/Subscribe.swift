471791dd488c5beb66a91e39eeb7fc6024aa5c3a
Jay Herron <jay@passivelogic.com>
2021-02-22 23:18:55 -0700

    Updates documentation to reflect implementation

--- Sources/GraphQL/Subscription/Subscribe.swift
+++ Sources/GraphQL/Subscription/Subscribe.swift
@@ -6,21 +6,20 @@ import NIO
 /**
  * Implements the "Subscribe" algorithm described in the GraphQL specification.
  *
- * Returns a Promise which resolves to either an AsyncIterator (if successful)
- * or an ExecutionResult (error). The promise will be rejected if the schema or
- * other arguments to this function are invalid, or if the resolved event stream
- * is not an async iterable.
+ * Returns a future which resolves to a SubscriptionResult containing either
+ * a SubscriptionObservable (if successful), or GraphQLErrors (error).
  *
  * If the client-provided arguments to this function do not result in a
- * compliant subscription, a GraphQL Response (ExecutionResult) with
- * descriptive errors and no data will be returned.
+ * compliant subscription, the future will resolve to a
+ * SubscriptionResult containing `errors` and no `observable`.
  *
  * If the source stream could not be created due to faulty subscription
- * resolver logic or underlying systems, the promise will resolve to a single
- * ExecutionResult containing `errors` and no `data`.
+ * resolver logic or underlying systems, the future will resolve to a
+ * SubscriptionResult containing `errors` and no `observable`.
  *
- * If the operation succeeded, the promise resolves to an AsyncIterator, which
- * yields a stream of ExecutionResults representing the response stream.
+ * If the operation succeeded, the future will resolve to a SubscriptionResult,
+ * containing an `observable` which yields a stream of GraphQLResults
+ * representing the response stream.
  *
  * Accepts either an object with named arguments, or individual arguments.
  */
@@ -77,7 +76,7 @@ func subscribe(
                     operationName: operationName
                 )
             }
-            return SubscriptionResult(observable: subscriptionObservable)
+            return SubscriptionResult(observable: subscriptionObservable, errors: sourceResult.errors)
         } else {
             return SubscriptionResult(errors: sourceResult.errors)
         }
@@ -88,21 +87,20 @@ func subscribe(
  * Implements the "CreateSourceEventStream" algorithm described in the
  * GraphQL specification, resolving the subscription source event stream.
  *
- * Returns a Promise which resolves to either an AsyncIterable (if successful)
- * or an ExecutionResult (error). The promise will be rejected if the schema or
- * other arguments to this function are invalid, or if the resolved event stream
- * is not an async iterable.
+ * Returns a Future which resolves to a SourceEventStreamResult, containing
+ * either an Observable (if successful) or GraphQLErrors (error).
  *
  * If the client-provided arguments to this function do not result in a
- * compliant subscription, a GraphQL Response (ExecutionResult) with
- * descriptive errors and no data will be returned.
+ * compliant subscription, the future will resolve to a
+ * SourceEventStreamResult containing `errors` and no `observable`.
  *
- * If the the source stream could not be created due to faulty subscription
- * resolver logic or underlying systems, the promise will resolve to a single
- * ExecutionResult containing `errors` and no `data`.
+ * If the source stream could not be created due to faulty subscription
+ * resolver logic or underlying systems, the future will resolve to a
+ * SourceEventStreamResult containing `errors` and no `observable`.
  *
- * If the operation succeeded, the promise resolves to the AsyncIterable for the
- * event stream returned by the resolver.
+ * If the operation succeeded, the future will resolve to a SubscriptionResult,
+ * containing an `observable` which yields a stream of event objects
+ * returned by the subscription resolver.
  *
  * A Source Event Stream represents a sequence of events, each of which triggers
  * a GraphQL execution for that event.
